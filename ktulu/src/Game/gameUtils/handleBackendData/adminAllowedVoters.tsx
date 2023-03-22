690ca50cf9d84729c35df2efe92e5741c4a98ee8
Scharmi <scharmi1500@gmail.com>
2023-03-22 18:38:17 +0100

    Finished rework

--- ktulu/src/Game/gameUtils/handleBackendData/adminAllowedVoters.tsx
+++ ktulu/src/Game/gameUtils/handleBackendData/adminAllowedVoters.tsx
@@ -1,6 +1,5 @@
 import * as Interfaces from 'interfaces/interfaces'
 export const adminAllowedVoters = ((gameData:any, setGameState: Function, props: any) => {
-    console.log("POTENCJALNE MIEJSCE BŁĘDU", props);
     setGameState((prevState: Interfaces.AdminGameState | Interfaces.GameState) => (
         {
             ...prevState,
