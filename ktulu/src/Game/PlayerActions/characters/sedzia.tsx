02b7630445ea2ad03ec1758d3e9cbb5104d5c92d
Scharmi <scharmi1500@gmail.com>
2023-03-22 22:31:01 +0100

    "Fixed pijany sędzia"

--- ktulu/src/Game/PlayerActions/characters/sedzia.tsx
+++ ktulu/src/Game/PlayerActions/characters/sedzia.tsx
@@ -1,7 +1,8 @@
 import * as Interfaces from 'interfaces/interfaces'
 
 export function sedzia(socket: any,  gameData: any, sentData: any) {
-    gameData.turnPlayer = "sędzia";
+    if(gameData.myData.characterName === "sędzia") gameData.turnPlayer = "sędzia";
+    if(gameData.myData.characterName === "pijany sędzia") gameData.turnPlayer = "pijany sędzia";
     //TODO dodać pijanego potem może
     gameData.setGameState((prevState:Interfaces.GameState) => ({
         ...prevState,
